841d4f7de04dcf9bfeb5d192db773be2178db265
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-09-08 20:40:57 +0200

    configured for allowing images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.magnific.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
