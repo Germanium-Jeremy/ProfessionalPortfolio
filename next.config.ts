12b21d94576207a2f8d29edb9acd71a901ddb30d
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2026-09-13 12:30:23 +0200

    resolved white theme and added avator github to hostnames

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'www.magnific.com',
+        protocol: "https",
+        hostname: "www.magnific.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
       },
     ],
   },
